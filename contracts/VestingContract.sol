80410b2b69fc4963cf0b9055037627d039435038
Borys Altynnyk <BorysAltynnyk@gmail.com>
2021-11-15 13:01:57 +0200

    tests are added, CLAIM logic corrected

--- contracts/VestingContract.sol
+++ contracts/VestingContract.sol
@@ -32,6 +32,15 @@ contract VestingContract {
     _;
     }
 
+    function getVestingPeriod() public view returns (uint256){
+        return vestingPeriod;
+    }
+
+    function getTokenBalance(address _address) public view returns (uint256){
+        return token.balanceOf(_address);
+    }
+    
+
     function setVestingPeriod(uint256 _vestingPeriod) external onlyOwner {
         vestingPeriod = _vestingPeriod;
     }
@@ -45,12 +54,15 @@ contract VestingContract {
     function claim() external {
         Recipient storage r = recipients[msg.sender];
         require(r.nReceivedPayment < numberOfPeriods, "All payments are paid");
-
-        if ((r.vestingStartDate + r.nReceivedPayment * vestingPeriod ) <= block.timestamp ){
-            uint256 _timePassed = block.timestamp - (r.vestingStartDate + r.nReceivedPayment * vestingPeriod);
-            uint256 _newPeriods = _timePassed % vestingPeriod - r.nReceivedPayment;
+        uint256 _timePassed = block.timestamp - (r.vestingStartDate + r.nReceivedPayment * vestingPeriod);
+        uint256 _newPeriods = _timePassed / vestingPeriod - r.nReceivedPayment;
+        // token.transfer(msg.sender, 40);
+        if (_newPeriods > 0 && _newPeriods < numberOfPeriods - r.nReceivedPayment ){
             r.nReceivedPayment += _newPeriods;
-            token.transfer(msg.sender, _newPeriods * r.totalTokensToShare / numberOfPeriods);
-        }
+            token.transfer(msg.sender, _newPeriods * 20);
+       }else {
+           token.transfer(msg.sender, (numberOfPeriods - r.nReceivedPayment) * 20);
+       }
+
     }
 }
--- test/VestingContractTest.js
+++ test/VestingContractTest.js
@@ -1,11 +1,82 @@
 const { expect } = require("chai");
 
+
 describe("Vesting contract", () => {
   it("should init default vesting period", async () => {
     const VestingContract = await ethers.getContractFactory("VestingContract");
     const contract = await VestingContract.deploy();
 
-    // await contract.deployed();
-    // expect(await contract.getVestingPeriod()).to.equal(5 * 24 * 3600);
+    await contract.deployed();
+    expect(await contract.getVestingPeriod()).to.equal(5 * 24 * 3600);
   });
+
+  it("should give tokens after first vesting period pasted", async () =>{
+    const VestingContract = await ethers.getContractFactory("VestingContract");
+    const contract = await VestingContract.deploy();
+    await contract.deployed();
+
+    const [_, addr1] = await ethers.getSigners();  
+    const oldBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    await contract.addRecipient(100, await addr1.getAddress());
+
+    //time travel 1 period
+    // suppose the current block has a timestamp of 01:00 PM
+    await ethers.provider.send("evm_increaseTime", [5*24*3600])
+    // await ethers.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
+
+    await contract.connect(addr1).claim()
+
+    const newBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    expect(newBalance - oldBalance).to.equal(100/5);
+  })
+
+  it("should give 2 portion if tokens after first claim if 2 periods are pasted", async()=>{
+    const VestingContract = await ethers.getContractFactory("VestingContract");
+    const contract = await VestingContract.deploy();
+    await contract.deployed();
+
+    const [_, addr1] = await ethers.getSigners();  
+    const oldBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    await contract.addRecipient(100, await addr1.getAddress());
+
+    //time travel 1 period
+    // suppose the current block has a timestamp of 01:00 PM
+    await ethers.provider.send("evm_increaseTime", [10*24*3600])
+    // await ethers.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
+
+    await contract.connect(addr1).claim()
+
+    const newBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    expect(newBalance - oldBalance).to.equal(100/5 * 2);
+
+  })
+
+  it("should NOT give more than 5 portion after more than max periods are passed", async()=>{
+    const VestingContract = await ethers.getContractFactory("VestingContract");
+    const contract = await VestingContract.deploy();
+    await contract.deployed();
+
+    const [_, addr1] = await ethers.getSigners();  
+    const oldBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    await contract.addRecipient(100, await addr1.getAddress());
+
+    //time travel 1 period
+    // suppose the current block has a timestamp of 01:00 PM
+    await ethers.provider.send("evm_increaseTime", [25*24*3600])
+    // await ethers.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
+
+    await contract.connect(addr1).claim()
+
+    const newBalance = await contract.getTokenBalance(await addr1.getAddress());
+
+    expect(newBalance - oldBalance).to.equal(100/5 *5);
+
+  })
+
+
 });
